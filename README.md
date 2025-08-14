29b55d9831e51af8569bf67798b32327c667b576
Manish Kumar <manish.login01@gmail.com>
2025-08-14 17:17:59 +0530

    Remove backend, frontend, and README

--- README.md
+++
@@ -1,5 +0,0 @@
-# TasteTrove
-
-TasteTrove is a social media platform for sharing recipes! It's like an online cookbook where you can discover and share delicious recipes with others.
-
-> Project is not yet completed.
--- backend/models/users.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, require: true },
-  email: { type: String, require: true },
-  password: { type: String, require: true },
-});
-
-export const User = mongoose.model("User", userSchema);
